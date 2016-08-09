248d2835b8da4168726e646ba3a3c21725869b87
Addler Huang <addler@sina.cn>
2016-08-09 17:41:38 +0800

    fix bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mixin = require('utils-merge');
+var mixin = require('merge-descriptors');
 var Promise = require('promise');
 var common = require('./lib/functions');
 
@@ -9,36 +9,77 @@ var params = {
 console.log('DEBUG');
 common.listfile(params.basedir + '/app/lib/').then(result =>{
     console.log('Start lib promise');
-    var lib_promise = [];
+    // var lib_promise = [];
+    // for (var key in result) {
+    //     if (result.hasOwnProperty(key)) {
+    //         console.log(result[key]);
+    //         lib_promise.push(
+    //             new Promise((resolve, reject) =>{
+    //                 lib_promise.push(common.loadlibrary(params.basedir + '/app/lib/' + result[key])
+    //                     .catch(error => {
+    //                         return Promise.resolve({'result':-1, 'error':error});
+    //                 }));
+    //             })
+    //         );
+    //     }
+    // }
+    console.log('Lib_promise');
     for (var key in result) {
         if (result.hasOwnProperty(key)) {
-            console.log(result[key]);
-            lib_promise.push(common.loadlibrary(params.basedir + '/app/lib/' + result[key]).catch(resolve({'result':-1, 'error':error})));
+            result[key] = params.basedir + '/app/lib/' + result[key];
         }
     }
-    console.log('Lib_promise');
-    Promise.all(lib_promise).then(value => {
-        var lib = {};
-        if (value instanceof Array){
-            for (var key in value) {
-                if (value.hasOwnProperty(key)) {
-                    if (value[k].result === -1){
-                        console.log(value[k]);
-                    }else{
-                        mixin(lib, value[k]);
-                    }
+    return Promise.all(result.map(common.loadlibrary));
+    // Promise.all(lib_promise).;
+}, error =>{
+    console.log("will error");
+    console.log(error);
+    Promise.reject({});
+}).then(value => {
+    console.log("all done");
+    var Lib = function(){
+
+    };
+    var lib = new Lib();
+    if (value instanceof Array){
+        for (var key in value) {
+            if (value.hasOwnProperty(key)) {
+                console.log(value[key]);
+                if (value[key].result === -1){
+                    console.log(value[key]);
+                }else{
+                    mixin(Lib, value[key].prototype, false);
                 }
             }
         }
-        console.log('LIB');
-        console.log(lib);
-        resolve(lib);
-    });
-}, error =>{
-    console.log(error)
-    resolve({});
-}).then(lib => {
+    }
+    lib.println();
+    console.log(lib);
+}).catch(error => {
+    console.log(error);
+});
+/*
+.all(lib_promise)
+.then(value => {
+    var lib = {};
+    if (value instanceof Array){
+        for (var key in value) {
+            if (value.hasOwnProperty(key)) {
+                if (value[k].result === -1){
+                    console.log(value[k]);
+                }else{
+                    mixin(lib, value[k]);
+                }
+            }
+        }
+    }
+    console.log('LIB');
+    console.log(lib);
+    resolve(lib);
+})
+.then(lib => {
     console.log('hahaha');
     console.log(lib);
     lib.println();
 });
+*/
